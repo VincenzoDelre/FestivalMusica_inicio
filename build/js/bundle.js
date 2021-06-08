51d0717f54edd2f05bbe08952581563188f0b0c0
vincenzo delre <delrealberto@gmail.com>
2021-06-08 07:00:18 +0200

    proyecto terminado

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,43 @@
-console.log('LISTO');
+document.addEventListener('DOMContentLoaded', function() {
+  scrollNav();
+
+  navegacionfija();
+});
+
+function navegacionfija() {
+
+  //Registrar el elemento observer
+  const observer = new IntersectionObserver ( function (entries) {
+
+    const barra = document.querySelector('.header');
+
+    if(entries[0].isIntersecting) {
+        barra.classList.remove('fijo');
+    } else {
+        barra.classList.add('fijo');
+    }
+
+  });
+
+  //Elemento a observar
+  observer.observe(document.querySelector('.sobre-festival'));
+}
+
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+  enlaces.forEach( function( enlace) { //diferencia entre map y foreach
+    enlace.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const seccion = document.querySelector(e.target.attributes.href.value);
+      seccion.scrollIntoView({
+        behavior:'smooth'
+      });
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   crearGaleria();
